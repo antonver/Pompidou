49223649eb1e9a25bef0a63ad2132093cc9a90d6
antonver <anton.verbovyi@gmail.com>
2024-11-21 12:58:24 +0100

    g

--- index_anton.js
+++ index_anton.js
@@ -7,16 +7,25 @@ const PORT = 3000;
 const mysql = require("mysql")
 app.use(express.static('public'));
 
-// Les exemples donnГ©s sont pour une connexion en local sur la fac
-// Le user/database et password sont Г  modifier...
+
 const connection = mysql.createConnection({
     host: "sql7.freemysqlhosting.net",
     user: "sql7746118",
     password: "Jgclfec5nT",
     database: "sql7746118",
-    connectTimeout: 1000000,
+    connectTimeout: 100000000,
     port: 3306,
 });
+
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "1111",
+//     database: "my_new_database",
+//     connectTimeout: 10000,
+//     port: 3306,
+//
+// });
 
 connection.connect(error => {
     if (error) throw error;
@@ -25,7 +34,14 @@ connection.connect(error => {
 
 
 app.get("/data", (req, res) => {
-let query = connection.query("SELECT * FROM users");
+    connection.query("SELECT * FROM TypesActivite", (error, results) => {
+        if (error) throw error;
+        res.json(results); // Отправляем полученные данные в формате JSON
+    });
+});
+
+app.get("/main", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
 })
 app.listen(PORT, () => {
     console.log(`Server est lancé: http://localhost:${PORT}`);
